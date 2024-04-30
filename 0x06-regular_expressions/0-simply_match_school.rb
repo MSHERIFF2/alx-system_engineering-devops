ac7214f391be4fcc8fbc02be3e62e6dff116a039
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-04-30 11:57:28 +0100

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,19 @@
 #!/usr/bin/env ruby
 # A regular expression that is simply matching School
-puts ARGV[0].scan(/School/).join
+
+def match_regex(string)
+  if string =~ /School/
+    puts "#{string}"
+  else
+    puts "#{string}"
+  end
+end
+
+if ARGV.empty?
+  puts "Usage: #{$0} <string>"
+  exit 1
+end
+
+string = ARGV[0]
+
+match_regex(string)
