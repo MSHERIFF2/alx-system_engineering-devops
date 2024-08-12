378ec130544c9bd94c06bad054c34d3621a3ddff
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-08-12 20:33:36 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,8 +4,8 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
